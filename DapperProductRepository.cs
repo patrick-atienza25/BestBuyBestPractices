2f2537243ff8404ea412b1496f56a0caa5eb7f92
patrick atienza <patienza25@gmail.com>
2022-11-21 09:38:01 -0800

    finished

---
+++ DapperProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products;");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using BestBuyBestPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+//var departmentRepo = new DapperDepartmentRepository(conn);
+
+//departmentRepo.InsertDepartments("Patrick's New Department");
+
+//var departments = departmentRepo.GetAllDepartments();
+
+//foreach (var department in departments)
+//{
+//    Console.WriteLine(department.DepartmentID);
+//    Console.WriteLine(department.Name);
+//}
+
+var productRepository = new DapperProductRepository(conn);
+var products = productRepository.GetAllProducts();
+foreach (var product in products)
+{
+    Console.WriteLine($"Product name: {product.Name}");
+    Console.WriteLine($"Product ID: {product.ProductID}");
+    Console.WriteLine($"Product price: {product.Price}");
+    Console.WriteLine($"Product on sale: {product.OnSale}");
+    Console.WriteLine($"Product stock level: {product.StockLevel}");
+    Console.WriteLine();
+    Console.WriteLine();
+}
